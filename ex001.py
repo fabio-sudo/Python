1483271a261ee9bf188ba3fe26348da880325f30
Fábio Henrique Trevezane <fabiotrevezane@gmail.com>
2022-10-23 23:06:28 -0300

    Pyton1.0

---
+++ ex001.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor.'))
+n2 = int(input('Digite segundo valor.'))
+
+soma = n1+n2
+
+#print('O valor da some é',soma)
+print('A soma entre {} mais {} é {}'.format(n1,n2,soma))
